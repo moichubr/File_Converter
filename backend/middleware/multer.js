2dafd99be4380d8859352b0b0ca86db43ccbdba0
moichubr <brunmoira@gmail.com>
2023-10-25 08:41:23 -0300

    new public folder

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer")
 
 function uploadFile () {
     const storage = multer.diskStorage({
-        destination:'../public/files',
+        destination:'./public/files',
         filename: function (_req, file, cb) {
           var extention = file.originalname.slice((file.originalname.lastIndexOf('.'))) //saco la extension del archivo para el nombre
           cb(null, Date.now() + extention) //date.now ayuda a generar un nombre único ya que toma la hora exacta con miliseg en que fue cargado el archivo
